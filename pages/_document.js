4248371592ce61cbe48df44a211145563f27428a
lucas r. c. <lucas_rc15@live.com>
2024-06-04 16:56:35 -0300

    Analytics implementado

--- pages/_document.js
+++ pages/_document.js
@@ -1,24 +1,35 @@
-import {Html, Head, Main, NextScript} from 'next/document';
-import Script from 'next/script';
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
-
-
-    return (
-        <Html>
-            <Head>
-                <link rel="stylesheet"
-                      href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&family=Poppins:wght@300;400;500;600;700&display=swap"
-                />
-
-            </Head>
-            <body>
-            <Main/>
-            <NextScript/>
-            <Script src="/js/splitting.min.js" strategy="beforeInteractive"/>
-            <Script src="/js/isotope.min.js" strategy="beforeInteractive"/>
-            <Script src="/js/fjGallery.min.js" strategy="beforeInteractive"/>
-            </body>
-        </Html>
-    )
+  return (
+    <Html>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&family=Poppins:wght@300;400;500;600;700&display=swap"
+        />
+        {/* Google Tag Manager */}
+        
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-KK8NV0W3NW');
+              `,
+          }}
+        />
+        {/* Fim do Google Tag Manager */}
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+        <Script src="/js/splitting.min.js" strategy="beforeInteractive" />
+        <Script src="/js/isotope.min.js" strategy="beforeInteractive" />
+        <Script src="/js/fjGallery.min.js" strategy="beforeInteractive" />
+      </body>
+    </Html>
+  );
 }

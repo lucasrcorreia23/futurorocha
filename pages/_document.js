dbaa73a06fafc466dc26dc8676c80601dac8251e
lucas r. c. <lucas_rc15@live.com>
2024-07-08 20:49:08 -0300

    ga4 teste

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
-import Script from "next/third-parties";
 
 
 export default function Document() {
